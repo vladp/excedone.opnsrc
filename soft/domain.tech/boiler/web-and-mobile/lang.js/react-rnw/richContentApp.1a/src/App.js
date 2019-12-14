d0df163dfc6e8a8ef34ae465e6042ad98c1354d7
v <v@localhost>
2019-12-13 19:48:44 -0500

    fixed markdownit import. published updated yarn.lock

--- soft/domain.tech/boiler/web-and-mobile/lang.js/react-rnw/richContentApp.1a/src/App.js
+++ soft/domain.tech/boiler/web-and-mobile/lang.js/react-rnw/richContentApp.1a/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { /*Image,*/ ScrollView, Text,View, Platform,   } from 'react-native';
 
 import Markdown from 'react-native-markdown-display';
-import MarkdownIt from 'markdown-it';
+import MarkdownIt from 'react-native-markdown-display/src/MarkdownIt';
+
 //import FitImage from 'react-native-fit-image'; 
 
 import { MD_TEST_DATA1, MD_TEST_DATA2 } from './testData_markdown';
